8b6efc718e5125e134cff613cc4bdb7ea02a33d3
Rusanov Andrey <andrey15rusanov@mail.ru>
2023-03-21 22:04:38 +0600

    wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww

--- src/js/index.js
+++ src/js/index.js
@@ -7,7 +7,7 @@ const buttonSeeMore__img = document.querySelector('.brands-button__img')
 buttonSeeMore.addEventListener('click', function () {
     if (cards.classList.contains('cards--height-content')) {
       cards.classList.remove('cards--height-content')
-      buttonSeeMore__text.textContent = 'Показать';
+      buttonSeeMore__text.textContent = 'Показать все';
       buttonSeeMore__img.classList.remove('brands-button__img--rotate');
     } else {
       cards.classList.add('cards--height-content')
@@ -34,7 +34,7 @@ let newSwiper = new Swiper('.swiper', {
 
 const burgerButton = document.querySelector(".burger-button"); 
 const acideMenu = document.querySelector(".acide-menu")
-const burgerButtonClose = document.querySelector(".burger-close-button");
+const burgerButtonClose = document.querySelector(".acide-close-button");
 
 burgerButton.addEventListener('click', function () {
     acideMenu.classList.remove('acide-menu--display-none') 
@@ -44,7 +44,42 @@ burgerButtonClose.addEventListener('click', function () {
     acideMenu.classList.add('acide-menu--display-none')
 });
 
+// _________________
+const buttonSeeMoreRep = document.querySelector(".brands-button--repair");
+const cardsRep = document.querySelector('.cards--repair');
+const buttonSeeMoreRep__text = document.querySelector('.brands-button__text--repair')
+const buttonSeeMoreRep__img = document.querySelector('.brands-button__img--repair')
 
+buttonSeeMoreRep.addEventListener('click', function () {
+  if (cardsRep.classList.contains('cards--height-content')) {
+    cardsRep.classList.remove('cards--height-content')
+    buttonSeeMoreRep__text.textContent = 'Показать все';
+    buttonSeeMoreRep__img.classList.remove('brands-button__img--rotate');
+  } else {
+    cardsRep.classList.add('cards--height-content')
+    buttonSeeMoreRep__text.textContent = 'Скрыть';
+    buttonSeeMoreRep__img.classList.add('brands-button__img--rotate');
+  }
+});
+
+// _____________________
+
+const text = document.querySelector('.content-block__text');
+const ButtonSeeMoreText = document.querySelector('.button');
+const ButtonSeeMoreText__text = document.querySelector(".button__text")
+const ButtonSeeMoreText__img = document.querySelector(".button__img")
+
+ButtonSeeMoreText.addEventListener('click', function () {
+  if(text.classList.contains('content-block--height-content')) {
+    text.classList.remove('content-block--height-content');
+    ButtonSeeMoreText__text.textContent = 'Читать далее';
+    ButtonSeeMoreText__img.classList.remove('brands-button__img--rotate');
+  } else {
+    text.classList.add('content-block--height-content');
+    ButtonSeeMoreText__text.textContent = 'Скрыть';
+    ButtonSeeMoreText__img.classList.add('brands-button__img--rotate');
+  }
+});
 
 
 
